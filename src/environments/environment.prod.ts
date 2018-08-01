42741c6ef276bfc64d00d10b753ffe1b7e78905c
Suchit Pongnumkul <suchit.p@gmail.com>
2018-08-01 20:25:06 +0700

    add firebase angular material

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAPcWQZlZlu-oKncDd8Ii2ZRT9Ze5_OGts",
+    authDomain: "fams-3d85d.firebaseapp.com",
+    databaseURL: "https://fams-3d85d.firebaseio.com",
+    projectId: "fams-3d85d",
+    storageBucket: "fams-3d85d.appspot.com",
+    messagingSenderId: "153728758416"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAPcWQZlZlu-oKncDd8Ii2ZRT9Ze5_OGts",
+    authDomain: "fams-3d85d.firebaseapp.com",
+    databaseURL: "https://fams-3d85d.firebaseio.com",
+    projectId: "fams-3d85d",
+    storageBucket: "fams-3d85d.appspot.com",
+    messagingSenderId: "153728758416"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
